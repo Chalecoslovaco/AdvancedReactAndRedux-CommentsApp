dc6d1d1043808ce3d556abeaef7869a474fb531e
Sergio García <sergiogarher@gmail.com>
2018-08-22 18:54:34 +0200

    Add textarea change test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -15,5 +15,15 @@ afterEach(() => {
 
 it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
-    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
+});
+
+it('has a text area that can be typed in', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'New comment' }
+    });
+
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('New comment');
 });
