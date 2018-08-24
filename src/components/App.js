6c3c5f41c297e7b01bb63d0a44eb1b3fbf59fc3a
Sergio García <sergiogarher@gmail.com>
2018-08-24 17:44:49 +0200

    Add renderHeader and renderButton + Wire up authReducer and changeAuth action creator in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
-export default () => {
-    return (
-        <div>
-            <Route path="/post" component={CommentBox} />
-            <Route path="/" exact component={CommentList} />
-        </div>
-    );
+class App extends Component {
+    renderButton() {
+        const text = this.props.auth ? 'Sign Out' : 'Sign In';             
+        return <button onClick={() => this.props.changeAuth(!this.props.auth)}>{text}</button>;
+    }
+
+    renderHeader(){
+        return (
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/post">Post a New CommentBox</Link>
+                </li>
+                <li>
+                    {this.renderButton()}
+                </li>            
+            </ul>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderHeader()}
+                <Route path="/post" component={CommentBox} />
+                <Route path="/" exact component={CommentList} />
+            </div>
+        );
+    }
 }
+
+function mapStateToProps({ auth }) {
+    return { auth };
+};
+
+export default connect(mapStateToProps, actions)(App);
