a64caa6412bc64ebbb960a06ef232182bfec2b82
Sergio García <sergiogarher@gmail.com>
2018-08-24 17:50:49 +0200

    Remove old unused import

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from 'actions';
-import { saveComment } from '../actions';
 
 class CommentBox extends Component {
     state = { comment: '' };
