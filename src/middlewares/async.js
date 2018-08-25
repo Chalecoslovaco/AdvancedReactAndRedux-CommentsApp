e1e2e3365b6feb3d1dfdc28fc18b2de4d23c857a
Sergio García <sergiogarher@gmail.com>
2018-08-25 15:48:50 +0200

    Add async middleware logic

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,5 +1,17 @@
-export default ({ dispatch }) =>
-    next =>
-        action => { 
-            
-        }
+export default ({ dispatch }) => next => action => {
+    //Check if the action has a promise on its payload  
+    if(!action.payload || !action.payload.then){
+        //If it doesn't send the action to the next middleware
+        return next(action);        
+    }
+
+    //If it does wait for the promise to resolve and then create a newaction with the data and dispatch it
+    action.payload.then((response) => {
+        //Create a new action with all the properties of the action passed to this middleware and then override 
+        //the payload property with the resolved promise data
+        const newAction = { ...action, payload: response };
+        
+        //Then dispatch the new action
+        dispatch(newAction);
+    });
+}
